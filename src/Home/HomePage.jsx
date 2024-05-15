d1ee261c9f351d76181a21794005c35ec48142b5
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-16 01:42:23 +0530

    add piechart

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -6,16 +6,24 @@ import {Plus} from "lucide-react"
 import { ModalWrapper } from '../component/Modal/ModalWrapper'
 import { BalanceForm } from '../component/BalanseForm/BalanceForm'
 import { ExpenseForm } from '../component/ExpenseForm/ExpenseForm'
+import PieChart from '../component/PieChart/PieChart'
 
 export const HomePage = () => {
 
     const [balance , setBalance] = useState(5300)
-    const expense  = 5600
+    const [expense , setExpense]  = useState(0)
     const theExpenseList = []
 
     const [isOpenBalance , setIsOpenBalance] = useState(false)
     const [isOpenExpense , setIsOpenExpense] = useState(false)
 
+    const [categorySpends , setCategorySpends] = useState({
+        food: 0,
+        entertainment: 0,
+        travel: 0,
+    })
+
+    
     if(!localStorage.getItem("wallet balance")){
         localStorage.setItem("wallet balance" , balance)
     }
@@ -25,7 +33,7 @@ export const HomePage = () => {
     if(!localStorage.getItem("expense")){
         localStorage.setItem("expense" , expense)
     }
-
+    
     const handleAddBalance = (data) => {
         localStorage.setItem("wallet balance" , data)
         setIsOpenBalance(false)
@@ -35,9 +43,41 @@ export const HomePage = () => {
         expenseList.push(data)
         localStorage.setItem("expenseList" , JSON.stringify(expenseList))
     }
-
     
-
+    useEffect(() => {
+        let foodSpends = 0,
+        entertainmentSpends = 0,
+        travelSpends = 0;
+        
+        
+        const expenseList = JSON.parse(localStorage.getItem('expenseList'))
+        if(expenseList.length > 0){
+            let total = expenseList.reduce(
+                (accumulator , currentValue) =>
+                    accumulator + Number(currentValue.price),
+                0
+            )
+            setExpense(total)
+            
+            expenseList.forEach((ele) => {
+                if(ele.category == "food"){
+                    foodSpends += Number(ele.price)
+                }else if(ele.category == "entertainment"){
+                    entertainmentSpends += Number(ele.price)
+                }else if(ele.category == "travel"){
+                    travelSpends += Number(ele.price)
+                }
+            })
+            
+            setCategorySpends({
+                food: foodSpends,
+                travel: travelSpends,
+                entertainment: entertainmentSpends,
+            })
+        }
+    } , [localStorage.getItem('expenseList')])
+    
+    console.log("object" , categorySpends);
     return (
         <div>
             <Section title={"Expense Tracker"} IsSection={"1"} >
@@ -52,10 +92,19 @@ export const HomePage = () => {
                 <Card 
                     title={"Expense"}
                     btnText={` Add Expense`}
-                    balance={localStorage.getItem("expense")}
+                    balance={expense}
                     icon = {<Plus/>}
                     isGreen = {false}
                     handleClick={() => setIsOpenExpense(true)}
+                />
+
+                <PieChart
+                
+                    data={[
+                        { name: "Food", value: categorySpends.food },
+                        { name: "Entertainment", value: categorySpends.entertainment },
+                        { name: "Travel", value: categorySpends.travel },
+                    ].filter(ele => ele.value)}
                 />
             </Section>
 
